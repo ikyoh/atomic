be96cbb27862d04a91787cc9d5797eaa6ca08037
ikyoh <myikyoh@gmail.com>
2025-11-10 14:52:45 +0100

    Update ProductCategoryCard.tsx

--- components/ProductCategoryCard.tsx
+++ components/ProductCategoryCard.tsx
@@ -4,9 +4,10 @@ import Link from "next/link";
 
 export function ProductCategoryCard({ category, breadcrumb = [], className, ...props }: { category: any, breadcrumb?: string[], className?: string }) {
 
-
     // fonction qui retourne le slug /produits/ + les valeurs concatenées de breadcrumb + category.slug
     const slug = breadcrumb.length > 0 ? `/produits/${breadcrumb.join('/')}/${category.slug}` : `/produits/${category.slug}`;
+
+    console.log('category', category)
 
     return (
         <Link href={slug} className={cn("p-3", className)}>
@@ -14,8 +15,7 @@ export function ProductCategoryCard({ category, breadcrumb = [], className, ...p
                 <Image
                     src={category.acf.thumbnail ? category.acf.thumbnail : '/not-found.png'}
                     alt={category.name}
-                    width={232}
-                    height={186}
+                    fill={true}
                     quality={85}
                     className="object-cover aspect-[calc(300/240)] transition-all group-hover:scale-110 m-0! rounded-none! bg-light"
                 />
